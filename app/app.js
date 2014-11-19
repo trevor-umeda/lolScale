63fea0239b833566e58878c100fd9cb0a51b6551
tumeda <tumeda@riotgames.com>
2014-11-19 07:46:56 -0800

    deleting dumb bootstrap stuff and working on real data

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var lolScaleApp = angular.module('lolScaleApp', []);
+
+lolScaleApp.controller('ChampionListCtrl', ['$scope', '$http', function ($scope, $http) {
+   $http.get("http://ddragon.leagueoflegends.com/cdn/3.14.41/data/en_US/champion.json").success(function(data){
+     console.log("asdfasdfs")
+      $scope.data = data
+   })
+
+}])
