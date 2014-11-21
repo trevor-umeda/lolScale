c8f01337fc8d2f402a23620a97ab571f4b850070
trevor-umeda <trevor.umeda@gmail.com>
2014-11-21 01:16:19 -0800

    adding character selection and stuffs

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var lolScaleApp = angular.module('lolScaleApp', []).directive('resize', function () {
+var lolScaleApp = angular.module('lolScaleApp', ['lolScaleApp.services']).directive('resize', function () {
 
     return {
       restrict: 'A',
@@ -11,11 +11,36 @@ var lolScaleApp = angular.module('lolScaleApp', []).directive('resize', function
       }
     };
   });
+angular.module('lolScaleApp.services',[]).factory('selectedChampionService',function(){
+    return{
+        selectionMade: false,
+        champSelection: '',
+        champData: {}
+    };
+});
 
-lolScaleApp.controller('ChampionListCtrl', ['$scope', '$http', function ($scope, $http) {
+
+lolScaleApp.controller('ChampionDetailsCtrl' , ['$scope', '$http', 'selectedChampionService', function( $scope, $http, selectedChampionService) {
+   $scope.selectedChampionService = selectedChampionService;
+    $scope.selectedLevel = 1;
+    var calculateStats = function(){
+        console.log($scope.selectedLevel);
+    };
+    $scope.$watch('selectedChampionService',calculateStats,true);
+
+}]);
+
+lolScaleApp.controller('ChampionListCtrl', ['$scope', '$http', 'selectedChampionService', function ($scope, $http, selectedChampionService) {
    $http.get("http://ddragon.leagueoflegends.com/cdn/3.14.41/data/en_US/champion.json").success(function(data){
-     console.log("asdfasdfs")
       $scope.data = data
-   })
+   });
+    $scope.selectedChampionService = selectedChampionService;
+    $scope.selectChampion = function(champion) {
+        $scope.selectedChampionService.champSelection = champion
+        $scope.selectedChampionService.selectionMade = true;
+        $http.get("http://ddragon.leagueoflegends.com/cdn/3.14.41/data/en_US/champion/" + champion + ".json").success(function(data){
+            $scope.selectedChampionService.champData = data.data[champion];
+        })
+    }
 
-}])
+}]);
